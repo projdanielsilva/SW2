c2a7894f2e94317b97362981bcd6c2741e1d7ec7
Daniel Da Silva <100931944+projdanielsilva@users.noreply.github.com>
2024-06-25 10:59:44 -0300

    Add files via upload

---
+++ aula_api/api/data.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    [
+        'nome' => 'Daniel',
+        'email' => 'daniel@gmail' 
+    ],
+    [
+        'nome' => 'Anderson',
+        'email' => 'Anderson@gmail' 
+    ],
+    [
+        'nome' => 'Stephanie',
+        'email' => 'tete@gmail' 
+    ],
+    [
+        'nome' => 'Danielle',
+        'email' => 'dani@gmail' 
+    ],
+    [
+        'nome' => 'Ariany',
+        'email' => 'ariany@gmail' 
+    ],
+    [
+        'nome' => 'Victor',
+        'email' => 'victor@gmail' 
+    ],
+    [
+        'nome' => 'Ana',
+        'email' => 'ana@gmail' 
+    ],
+    [
+        'nome' => 'Lucas',
+        'email' => 'lucas@gmail' 
+    ],
+    [
+        'nome' => 'Maria Rita',
+        'email' => 'maria.rita@gmail' 
+    ],
+    [
+        'nome' => 'Thifany',
+        'email' => 'thifao@gmail' 
+    ],
+
+];
+
+?>
